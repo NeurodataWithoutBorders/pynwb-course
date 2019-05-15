6071e57f94bc7bb0c75c9ef23454b73624bf0c56
Ryan Ly <rly@lbl.gov>
2019-05-15 00:42:33 -0400

    add more stringest test for exc_01_02

--- exercises/test_01_02.py
+++ exercises/test_01_02.py
@@ -1,2 +1,5 @@
 def test():
+    # only check that the code runs and nwbfile.identifier is somewhere in the solution
+    assert "nwbfile.identifier" in __solution__.splitlines()[-1], "Are you printing the session identifier?"
+
     __msg__.good("Well done!")
