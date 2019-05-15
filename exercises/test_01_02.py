709c4f06c361117c38fec79af369ac9d14e8abae
Ryan Ly <rly@lbl.gov>
2019-05-15 00:52:29 -0400

    update solution for test 01 02

--- exercises/test_01_02.py
+++ exercises/test_01_02.py
@@ -1,5 +1,5 @@
 def test():
-    # only check that the code runs and nwbfile.identifier is somewhere in the solution
-    assert "nwbfile.identifier" in __solution__.splitlines()[-1], "Are you printing the session identifier?"
+    # only check that the code runs and nwbfile.identifier is in the last line of the solution
+    assert "nwbfile.identifier" in __solution__.strip().splitlines()[-1], "Are you printing the session identifier?"
 
     __msg__.good("Well done!")
